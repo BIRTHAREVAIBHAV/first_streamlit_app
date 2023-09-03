a38f073e20ec07fe6a9f281241e096c4e97bc3b9
BIRTHAREVAIBHAV <143940954+BIRTHAREVAIBHAV@users.noreply.github.com>
2023-09-03 20:27:30 +0800

    Update streamlit_app.py
    removed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My first streamlit code)
+streamlit.title('My first streamlit code')
 streamlit.header('Snoflake Data Application Builders Workshop)
 streamlit.text('Code will start here...')
 streamlit.text('Breakfast Menu')
