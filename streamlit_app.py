d95df6a7aa6360896fa1a61c77b943cdda7bf2ca
BIRTHAREVAIBHAV <143940954+BIRTHAREVAIBHAV@users.noreply.github.com>
2023-09-03 20:28:05 +0800

    Update streamlit_app.py
    removed another error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My first streamlit code')
-streamlit.header('Snoflake Data Application Builders Workshop)
+streamlit.header('Snoflake Data Application Builders Workshop')
 streamlit.text('Code will start here...')
 streamlit.text('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
