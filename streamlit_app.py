d8ebe0fa9861eeb64a4404caf406bf0b05e9ab38
BIRTHAREVAIBHAV <143940954+BIRTHAREVAIBHAV@users.noreply.github.com>
2023-09-03 20:26:58 +0800

    Update streamlit_app.py
    added breakfast menu and 3 items.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
-streamlit.title('My Parents New Healty Dinner)
-streamlit.header('Breakfast Menu')
+streamlit.title('My first streamlit code)
+streamlit.header('Snoflake Data Application Builders Workshop)
+streamlit.text('Code will start here...')
+streamlit.text('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
